fb251197fc5a53a7314d62dad13db6e24962e6aa
Rob41k <olekssisnegoda2@gmail.com>
2025-07-05 01:58:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ def home():
     return "Bot is running"
 
 def run_flask():
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK")
 RSS_URL = "https://rsshub.app/telegram/channel/gruntmedia"
